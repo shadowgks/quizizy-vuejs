ca8867100aa741643b6b6e9a6908c8f5b761821e
shadowgks <saad.moumou.coder@gmail.com>
2023-03-15 09:58:44 +0100

    done

--- src/components/Answers.vue
+++ src/components/Answers.vue
@@ -76,4 +76,20 @@ export default {
     overflow-y: scroll;
     height: 25rem;
 }
+.answers_body::-webkit-scrollbar-track
+{
+	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+	background-color: #F5F5F5;
+}
+
+.answers_body::-webkit-scrollbar
+{
+	width: 6px;
+	background-color: #F5F5F5;
+}
+
+.answers_body::-webkit-scrollbar-thumb
+{
+	background-color: #3F0071;
+}
 </style>
